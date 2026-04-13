2ecdef2f300bbdb605d33de38e904c77ad544f31
Anjali Jadhav <jadhavanjali@gmail.com>
2026-04-13 22:48:50 +0530

    password strength check

---
+++ Level2/code88.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+
+        // Password pattern
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$";
+
+        if (Regex.IsMatch(password, pattern))
+        {
+            Console.WriteLine("Strong Password");
+        }
+        else
+        {
+            Console.WriteLine("Weak Password");
+        }
+    }
+}
